5326d4eddeae460c610acebcb9e64fe30026fd23
Fanterlum <vicente.gonzalez5380@alumnos.udg.mx>
2023-01-22 20:51:38 -0600

    este es el mini generador lexico, solo lee espacios, identificadores y reales

--- Form1.cs
+++ Form1.cs
@@ -93,34 +93,8 @@ namespace compilador1
                 if (
 
 
-                    token1 == " " |
-                    token1 == "int" |
-                    token1 == "float" |
-                    token1 == "void" |
-                    token1 == "<" |
-                    token1 == "<=" |
-                    token1 == ">" |
-                    token1 == ">=" |
-                    token1 == "||" |
-                    token1 == "&&" |
-
-                    token1 == "!=" |
-                    token1 == ";" |
-                    token1 == "," |
-                    token1 == "(" |
-                    token1 == ")" |
-                    token1 == "{" |
-                    token1 == "}" |
-                    token1 == "if" |
-                    token1 == "while" |
-                    token1 == "return" |
-                    token1 == "else" |
-                    token1 == "$" |
-                    token1 == "+" |
-                    token1 == "-" |
-                    token1 == "/" |
-                    token1 == "*"  //este lo agregue extra
-
+                    token1 == " " 
+                    
                         )
                 {
 
@@ -131,38 +105,10 @@ namespace compilador1
                     variables = "";
 
                 }
-                else if (token1 == "=")
-                {
+                
 
 
-                    if (saber_espacio == "=")
-                    {
-
-                        txtSalida.Text = txtSalida.Text + "==" + " " + "opIgualdad = 11\n";
-                        tokenllevado = dataLexemas.Rows.Add();
-                        dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = token1+"=";
-                        dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "11";
-                        token1 = "";
-                        gg = true;
-                        variables = "";
-                        inicioestado++;
-                        
-
-                    }
-                    else
-                    {
-                        txtSalida.Text = txtSalida.Text + "=" + " " + "= 18\n";
-                        tokenllevado = dataLexemas.Rows.Add();
-                        dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = token1;
-                        dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "18";
-
-                        token1 = "";
-                        gg = true;
-                        variables = "";
-                    }
-
-
-                }
+                
                 else if (variables.Length > 0 && ( saber_espacio == " "))
                 {
 
@@ -221,90 +167,7 @@ namespace compilador1
         {
             switch (tokeniguala)
             {
-            
 
-                    //lo agregue
-                case "int":
-                    txtSalida.Text = txtSalida.Text + "int" + " " + "tipo = 6\n";
-                    break;
-                case "float":
-                    txtSalida.Text = txtSalida.Text + "float" + " " + "tipo = 6\n";
-                    break;
-                case "void":
-                    txtSalida.Text = txtSalida.Text + "void" + " " + "tipo = 6\n";
-                    break;
-                case "+":
-                    txtSalida.Text = txtSalida.Text + "+" + " " + " opSuma = 5\n";
-                    break;
-                case "-":
-                    txtSalida.Text = txtSalida.Text + "-" + " " + "opSuma = 5\n";
-                    break;
-                case "/":
-                    txtSalida.Text = txtSalida.Text + "/" + " " + "opMul = 6\n";
-                    break;
-                case "*":
-                    txtSalida.Text = txtSalida.Text + "*" + " " + "opMul = 6\n";
-                    break;
-
-                case "<":
-                    txtSalida.Text = txtSalida.Text + "<" + " " + "opRelac = 7\n";
-                    break;
-                case "<=":
-                    txtSalida.Text = txtSalida.Text + "<=" + " " + "opRelac = 7\n";
-                    break;
-                case ">":
-                    txtSalida.Text = txtSalida.Text + ">" + " " + "opRelac = 7\n";
-                    break;
-                case ">=":
-                    txtSalida.Text = txtSalida.Text + ">=" + " " + "opRelac = 7\n";
-                    break;
-
-                case "||":
-                    txtSalida.Text = txtSalida.Text + "||" + " " + "opOr = 8\n";
-                    break;
-                case "&&":
-                    txtSalida.Text = txtSalida.Text + "&&" + " " + "opAnd = 9\n";
-                    break;
-                case "!":
-                    txtSalida.Text = txtSalida.Text + "!" + " " + "opNot = 10\n";
-                    break;
-
-                case "!=":
-                    txtSalida.Text = txtSalida.Text + "!=" + " " + "opIgualdad = 11\n";
-                    break;
-                case ";":
-                    txtSalida.Text = txtSalida.Text + ";" + " " + "= 12\n";
-                    break;
-                case ",":
-                    txtSalida.Text = txtSalida.Text + "," + " " + "= 13\n";
-                    break;
-                case "(":
-                    txtSalida.Text = txtSalida.Text + "*(*" + " " + "= 14\n";
-                    break;
-                case ")":
-                    txtSalida.Text = txtSalida.Text + "*)*" + " " + "= 15\n";
-                    break;
-                case "{":
-                    txtSalida.Text = txtSalida.Text + "*{*" + " " + "= 16\n";
-                    break;
-                case "}":
-                    txtSalida.Text = txtSalida.Text + "*}*" + " " + "= 17\n";
-                    break;
-                case "if":
-                    txtSalida.Text = txtSalida.Text + "if" + " " + "= 19\n";
-                    break;
-                case "while":
-                    txtSalida.Text = txtSalida.Text + "while" + " " + "= 20\n";
-                    break;
-                case "return":
-                    txtSalida.Text = txtSalida.Text + "return" + " " + "= 21\n";
-                    break;
-                case "else":
-                    txtSalida.Text = txtSalida.Text + "else" + " " + "= 22\n";
-                    break;
-                case "$":
-                    txtSalida.Text = txtSalida.Text + "$" + " " + "= 23\n";
-                    break;
                 case " ":
                     txtSalida.Text = txtSalida.Text + "(espacio)" + " " + "= 24\n";
                     break;
@@ -324,116 +187,6 @@ namespace compilador1
             switch (lexema)
             {
 
-                    //agregado
-
-                case "int":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "4";
-                    break;
-                case "void":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "4";
-                    break;
-                case "float":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "4";
-                    break;
-                case "-":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "5";
-                    break;
-                case "+":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "5";
-                    break;
-                case "*":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "6";
-                    break;
-                case "/":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "6";
-                    break;
-
-                case "<":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "7";
-                    break;
-                case "<=":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "7";
-                    break;
-                case ">":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "7";
-                    break;
-                case ">=":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "7";
-                    break;
-                case "||":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "8";
-                    break;
-                case "&&":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "9";
-                    break;
-                case "!":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "10";
-                    break;
-
- 
-                case "!=":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "11";
-                    break;
-
-                case ";":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "12";
-                    break;
-                case ",":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "13";
-                    break;
-                case "(":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "14";
-                    break;
-                case ")":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "15";
-                    break;
-                case "{":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "16";
-                    break;
-                case "}":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "17";
-                    break;
-                case "if":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "19";
-                    break;
-                case "while":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "20";
-                    break;
-                case "return":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "21";
-                    break;
-                case "else":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "22";
-                    break;
-                case "$":
-                    dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
-                    dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "23";
-                    break;
                 case " ":
                     dataLexemas.Rows[tokenllevado].Cells["Lexema"].Value = lexema;
                     dataLexemas.Rows[tokenllevado].Cells["Token"].Value = "24";
